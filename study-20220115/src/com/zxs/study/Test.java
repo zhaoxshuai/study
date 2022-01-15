51590f851e9ccc7b56a0546b370c23a931ef0650
zhaoxshuai <746185810@qq.com>
2022-01-15 15:01:00 +0800

    add study-20220115 module

---
+++ study-20220115/src/com/zxs/study/Test.java
@@ -0,0 +1,12 @@
+package com.zxs.study;
+
+/**
+ * @author zxs
+ * @date 2022/1/15 下午2:59
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
