4528ea4db9a7739d4c54b1ede3798488a80a556c
zhaoxshuai <746185810@qq.com>
2023-04-18 23:34:14 +0800

    update study-20220115 module

---
+++ farming-demo/src/main/java/com/study/farmingdemo/FarmingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.study.farmingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmingDemoApplication.class, args);
+    }
+
+}
---
+++ farming-demo/src/test/java/com/study/farmingdemo/FarmingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.farmingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
